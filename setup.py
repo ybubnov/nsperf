b3292daddacff08deee3a17570a084b00db9ea04
Yasha Bubnov <girokompass@gmail.com>
2015-11-15 00:57:12 +0300

    autoinstall dependencies, LINC-Switch testbed

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ setup(
     version='0.0.1',
     description="Utility to generate DNS packages",
     packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "dnspython3==1.12.0",
+    ],
     entry_points={
         "console_scripts": ["nsperf = nsperf.shell.run:main"],
     },
-    zip_safe=False,
 )
